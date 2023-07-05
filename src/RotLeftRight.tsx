c1f3bc5fb34dabf7b19025ae177bbd7ed664e19e
Anwesh43 <anweshthecool0@gmail.com>
2023-07-05 16:49:04 +0530

    RotLeftRight component

---
+++ src/RotLeftRight.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import withContext from "./withContext";
+import { useStyle } from "./hooks";
+
+interface RTLProps {
+    w : number,
+    h : number, 
+    cb : () => void, 
+    scale : number
+}
+
+const RotLeftRight : React.FC<RTLProps> = (props : RTLProps)  => {
+    const {parentStyle, barStyle} = useStyle(props.w, props.h, props.scale)
+    return (<div style = {parentStyle()}>
+        <div style = {barStyle()}>
+        </div>
+    </div>)
+}
+
+export default withContext(RotLeftRight)
